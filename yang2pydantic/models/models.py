233f1fbd3a744d9eb3246ea78fbae07bf9ff39aa
Dominic <dominic.walther@ost.ch>
2022-10-21 01:36:21 +0200

    Somewhat functional prototype

--- yang2pydantic/models/models.py
+++ yang2pydantic/models/models.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
+from operator import truediv
+from sre_parse import State
 
-from typing import Any, Dict, List, Tuple, Type
+from typing import Annotated, Any, Dict, List, Optional, Tuple, Type
 
 from pyang.context import Context
 from pyang.statements import (
@@ -13,8 +15,9 @@ from pyang.statements import (
 )
 from pyang.types import TypeSpec
 from pydantic import BaseConfig, BaseModel, create_model
+from pydantic.class_validators import validator
 from pydantic.config import Extra
-from pydantic.fields import ModelField
+from pydantic.fields import Field, ModelField
 from pydantic.types import conint, constr
 
 # https://network.developer.nokia.com/sr/learn/yang/understanding-yang/
@@ -26,6 +29,11 @@ class NotImplementedException(Exception):
 
 
 class PyangStatement(BaseModel):
+
+    _raw_statement: Annotated[Statement, Field(exclude=True)]
+    arg: Annotated[str, Field(exclude=True)] = ''
+    children: Annotated[List[PyangStatement|Type[PyangStatement]], Field(exclude=True)] = []
+
     def __init__(
         self,
         stm: Statement,
@@ -35,27 +43,23 @@ class PyangStatement(BaseModel):
     ) -> None:
         assert isinstance(stm, Statement)
         # Print where the current data is coming from ASAP
-        super().__init__()
-        self._raw_statement: Statement = stm
-        self.keyword = stm.keyword
-        self.pos = stm.pos
-        print(f'Creating "{self.keyword}" read from "{self.pos.ref}:{self.pos.line}"')
-
+        print(f'Creating "{stm.keyword}" read from "{stm.pos.ref}:{stm.pos.line}"')
+        children=PyangStatement.extract_statement_list(stm, 'i_children') if resolve_children else []
+        super().__init__(
+            arg=stm.arg,
+            _raw_statement=stm,
+            children=children,
+        )
+        # self.pos: Annotated[Any, Field(exclude=True)] = stm.pos
+        # print(f'Creating "{self.keyword}" read from "{self.pos.ref}:{self.pos.line}"')
         # Load the rest of the values
-        self.children = PyangStatement.extract_statement_list(stm, 'i_children') if resolve_children else []
-        self.substmts = PyangStatement.extract_statement_list(stm, 'substmts') if resolve_substatements else []
-
-        self.comments = " ".join((c.arg.lstrip('/ \n') for c in (stm.search('_comment'))))
-        self.arg: str = stm.arg
-        self.top = stm.top
-        self.parent = stm.parent
-        self.raw_keyword = stm.raw_keyword
-        self.ext_mod = stm.ext_mod
-        assert self.keyword == self.raw_keyword
+        # self.substmts: Annotated[PyangStatement, Field(exclude=True)] = PyangStatement.extract_statement_list(stm, 'substmts') if resolve_substatements else []
+        # self.typedefs: Annotated[Dict[str, TypedefStatement], Field(exclude=True)] = stm.i_typedefs
+        # self.comments = " ".join((c.arg.lstrip('/ \n') for c in (stm.search('_comment'))))
 
     class Config:
         arbitrary_types_allowed = True
-        extra = Extra.allow
+        # extra = Extra.allow
 
     def to_pydantic_schema(self) -> Type[BaseModel]:
         return create_model(self.arg, __base__=BaseModel, **{})
@@ -83,6 +87,7 @@ class PyangStatementFactory:
         def __call__(self, *args: Any, **kwds: Any) -> Any:
             return self.maps_to(*args, **kwds)
 
+    known_types: Dict[str, Type[PyangStatement]] = dict()
     _implemented_mappings: Dict[str, ClassMapping] = {}
 
     @classmethod
@@ -102,8 +107,16 @@ class PyangStatementFactory:
         if stm.keyword not in cls._implemented_mappings.keys():
             cls.register_statement_class([stm.keyword])(GenericStatement)
             print(f'"{stm.keyword}" has not yet been implemented as a type.')
-        a = cls._implemented_mappings[stm.keyword](stm)
+        mapping = cls._implemented_mappings[stm.keyword]
+        a = mapping.maps_to(stm)
         return a
+
+    @staticmethod
+    def register_type_definition(type_name: str, type: PyangType):
+        if __class__.known_types.get(type_name, None) is None:
+            __class__.known_types[type_name] = type
+        else:
+            raise Exception(f"Type already registered. Possible duplicate type name: '{type_name}'")
 
 
 @PyangStatementFactory.register_statement_class(['type'])
@@ -162,32 +175,26 @@ class FieldConfig(BaseConfig):
 
 
 class PyangModule(PyangStatement):
-    class Serializable(BaseModel):
-        version: constr(regex='^1$')  # Only PYANG RFC 6020 supported for now.
-        prefix: constr(min_length=1)
-        latest_revision: str | None
-        # identities: Dict[str, Statement] = {}
+    #typedefs: Annotated[Dict[str, TypedefStatement], Field(exclude=True)] = {}
+    version: constr(regex='^1$') = '1'  # Only PYANG RFC 6020 supported for now.
+    latest_revision: str = ''
 
     def __init__(self, module: ModSubmodStatement) -> None:
         assert isinstance(module, ModSubmodStatement)
         super().__init__(module, resolve_children=True, resolve_substatements=True)
-        self.prefixes: Dict[str, Tuple[str, Any]] = module.i_prefixes
-        self.unused_prefixes = module.i_unused_prefixes
-        self.missing_prefixes = module.i_missing_prefixes
-        self.modulename: str = module.i_modulename
-        self.features = module.i_features
-        self.extensions = module.i_extensions
-        self.including_modulename = module.i_including_modulename
-        self.ctx: Context = module.i_ctx
-        self.undefined_augment_nodes = module.i_undefined_augment_nodes
-        self.is_primary_module: bool = module.i_is_primary_module
-        self.serializable = __class__.Serializable(
-            version=module.i_version,
-            prefix=module.i_prefix,
-            latest_revision=module.i_latest_revision,
-            children=self.children,
-            # identities=module.i_identities,
-        )
+        # self.prefixes: Dict[str, Tuple[str, Any]] = module.i_prefixes
+        # self.unused_prefixes = module.i_unused_prefixes
+        # self.missing_prefixes = module.i_missing_prefixes
+        # self.modulename: str = module.i_modulename
+        # self.features = module.i_features
+        # self.extensions = module.i_extensions
+        # self.including_modulename = module.i_including_modulename
+        # self.ctx: Context = module.i_ctx
+        # self.undefined_augment_nodes = module.i_undefined_augment_nodes
+        # self.is_primary_module: bool = module.i_is_primary_module
+        #self.typedefs = module.i_typedefs
+        self.version: constr(regex='^1$') = module.i_version  # Only PYANG RFC 6020 supported for now.
+        self.latest_revision: str | None
 
     def create_model_field(self, name, value) -> ModelField:
         return ModelField(
@@ -200,14 +207,12 @@ class PyangModule(PyangStatement):
         )
 
     def to_pydantic_schema(self) -> str:
-        fields_ = self.serializable.__fields__
+        fields_ = self.__fields__  # .serializable.__fields__
         fields: Dict[str, Tuple[type, Any]] = dict()  # name : (type, default_value)
         for name in fields_.keys():
-            if self.serializable.__class__.__fields__[name].required:
-                fields[name] = (fields_[name].type_, ...)
-            else:
-                val = self.serializable.__class__.__fields__[name].get_default()
-                fields[name] = (fields_[name].type_, val)
+            default = self.__class__.__fields__[name].get_default()
+            type = fields_[name].type_
+            fields[name] = (type, default if default is not None else ...)
         a = create_model(f"{self.arg}Module", __config__=FieldConfig, **fields)
         b = a.schema_json()
         return b
