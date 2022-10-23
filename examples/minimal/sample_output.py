49f6236bbfeb30b9c2632148214a273ff0f70121
Dominic <dominic.walther@ost.ch>
2022-10-23 12:58:40 +0200

    Add missed output field

--- examples/minimal/sample_output.py
+++ examples/minimal/sample_output.py
@@ -12,6 +12,7 @@ class InterfaceLeaf(BaseModel):
         regex=r'(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
     )
     subnet_mask: str
+    enabled: bool = False
 
 
 class InterfaceList(List[InterfaceLeaf]):
