af468f3609f190b143adc49f87ecdbbb13bea40e
Dominic <dominic.walther@ost.ch>
2022-10-23 13:08:46 +0200

    Make sample fully equivalent

--- examples/minimal/sample_output.py
+++ examples/minimal/sample_output.py
@@ -1,18 +1,28 @@
 from __future__ import annotations
 
-from typing import List
+from typing import Annotated, List
 
 from pydantic import BaseModel
+from pydantic.fields import Field
 from pydantic.types import constr
 
 
-class InterfaceLeaf(BaseModel):
-    name: str
-    address: constr(
+class DottedQuadType(
+    constr(
         regex=r'(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
     )
-    subnet_mask: str
-    enabled: bool = False
+):
+    """Four octets written as decimal numbers and separated with the '.' (full stop) character."""
+
+
+class InterfaceLeaf(BaseModel):
+    name: Annotated[str, Field(description="Interface name. Example value: GigabitEthernet 0/0/0")]
+    address: Annotated[
+        DottedQuadType,
+        Field(description="Interface IP address. Example value: 10.10.10.1"),
+    ]
+    subnet_mask: Annotated[str, Field(description="Interface subnet mask. Example value: 255.255.255.0")]
+    enabled: bool = Field(False, description="Enable or disable the interface. Example value: true")
 
 
 class InterfaceList(List[InterfaceLeaf]):
