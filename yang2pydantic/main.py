de79f705dded03618dafa9e14fb2727e6a5308b3
Dominic <dominic.walther@ost.ch>
2022-11-04 16:59:01 +0100

    Remove dead sample code

--- yang2pydantic/main.py
+++ yang2pydantic/main.py
@@ -12,18 +12,6 @@ logging.basicConfig(
 logger = logging.getLogger('pydantify')
 
 
-def fetch(address: str, port: int) -> json:
-    import requests
-    from requests.auth import HTTPBasicAuth
-    USER = 'arista'
-    PASS = 'arista'
-    headers = {'Content-Type': 'application/yang-data+json', 'Accept': 'application/yang-data+json'}
-    api_call = f"https://{address}:{port}/restconf/data/openconfig-interfaces:interfaces/interface=Ethernet1/state"
-    result: requests.Response = requests.get(api_call, auth=HTTPBasicAuth(USER, PASS), headers=headers, verify=False)
-    result.raise_for_status()
-    return result.json()
-
-
 def main():
     from .run import run
     from pathlib import Path
